57128049c47593ec54fe05139cdf03f2ebf6afc3
truemartinosss <75758821+truemartinosss@users.noreply.github.com>
2022-04-18 19:58:30 +0200

    Update chess.py

--- chess.py
+++ chess.py
@@ -1,10 +1,11 @@
 def main():
+    print("enter your file path \n")
+    file_path = input()
     try:
-        print("enter your file path \n")
-        file_path = input()
         with open(file_path) as dot:
             contents = dot.read()
             print(contents)
+            dot.close()
     except Exception as e:
         print("cannot read file: " + file_path)
         print(e)
